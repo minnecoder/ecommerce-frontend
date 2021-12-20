f5322dcf94520eb4fdc9eb2d8f32a2cde70a5347
minnecoder <sdparkin@protonmail.com>
2021-12-19 23:00:43 -0600

    Added several routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from '../src/pages/Login'
+import Register from './pages/Register';
+import Shop from './pages/Shop'
+import VerifyEmail from './pages/VerifyEmail'
+import AwaitEmailVerify from './pages/AwaitEmailVerify';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Login />} />
+        <Route index element={<Shop />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/verify/:email/:token" element={<VerifyEmail />} />
+        <Route path="/awaitemailverify" element={<AwaitEmailVerify />} />
       </Routes>
     </BrowserRouter>
   );
